bb7eb1f9bf05b5f500b07ce1802690b4b3b997ae
BlindPenguin <f.holzner@limeonfire.de>
2022-04-27 20:42:14 +0200

    Some commenting and adding it to base.py settings

--- core/articles/models.py
+++ core/articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.api import APIField
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+
+# Articles are meant to be used as basic pages.
+# They're called "Articles", because "Page" is already taken by Wagtail and "BasePage" sounds dumb.
+class Article(Page):
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
+
+    api_fields = [
+        APIField('body')
+    ]
