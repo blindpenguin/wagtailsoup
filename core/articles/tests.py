5601727e2c0f642f1ec6f5a9e6de073b4293b7d7
BlindPenguin <f.holzner@limeonfire.de>
2022-04-27 20:42:31 +0200

    Failing miserably at writing tests

--- core/articles/tests.py
+++ core/articles/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils.form_data import nested_form_data, rich_text
 
 # Create your tests here.
+from core.articles.models import Article
+from home.models import HomePage
+
+
+class ArticleTests(WagtailPageTests):
+    def test_can_create_an_article(self):
+        root_page = HomePage.objects.get(pk=2)
+
+        self.assertCanCreate(root_page, Article, nested_form_data({
+            'title': 'Hello World',
+            'body': rich_text('<p>This is <strong>Rich Text</strong>.</p>')
+        }))
