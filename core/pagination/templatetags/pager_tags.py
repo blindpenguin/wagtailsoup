ec011e8c8fb48c435ad3e74ba297639d8331f584
BlindPenguin <f.holzner@limeonfire.de>
2022-04-30 20:20:37 +0200

    Implementing pagination template

---
+++ core/pagination/templatetags/pager_tags.py
@@ -0,0 +1,12 @@
+from urllib.parse import parse_qs, urlparse
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
